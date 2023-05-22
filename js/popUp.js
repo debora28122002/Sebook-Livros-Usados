6b05698423b89b4868a375cc0a33aed4c905df06
debora28122002 <dbrdejesus15@gmail.com>
2023-05-22 17:50:24 -0300

    mais telas

--- js/popUp.js
+++ js/popUp.js
@@ -32,3 +32,25 @@ function popUpUserDono() {
     textConfirmPopUp.appendChild(linkElement);
   } 
 }
+
+function Book(){
+  var idBook = localStorage.getItem('idBook');
+  var fotoBook = document.getElementById('fotoBook');
+  var nomeBook = document.getElementById('nomeBook');
+  var caracteristicas = document.getElementById('caracteristicas');
+  var imgElement = document.createElement('img');
+
+  if(idBook === 'book1'){
+    nomeBook.textContent = 'Vida e Morte: Crespúsculo Reimaginado';
+    var texto = 'Autor(a): Stephenie Meyer<br>Ano: 2015<br>Unidades: 3<br>Preço: R$ 30,50';
+    caracteristicas.innerHTML = texto;
+    imgElement.src = 'img/crepusculo.png';
+    fotoBook.appendChild(imgElement);
+  } else if(idBook === 'book2'){
+    nomeBook.textContent = 'Harry Potter e o Cálice de Fogo';
+    var texto = 'Autor(a): J.K.Rowling<br>Ano: 2000<br>Unidades: 5<br>Preço: R$ 20,00';
+    caracteristicas.innerHTML = texto;
+    imgElement.src = 'img/harryp.png';
+    fotoBook.appendChild(imgElement);
+  }
+}
